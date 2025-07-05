16d3e395b62725f295a69e5393455e6327400d9e
b27user12 <b27user12@rpsconsulting.in>
2025-07-06 03:57:47 +0530

    5

--- frontend.sh
+++ frontend.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
+
 source ./common.sh
-app_name=frontend
 checkroot
+
 dnf module disable nginx -y &>>$logfile
-validate $? "disable default nginx"
+validate $? "Disabling Default Nginx"
 
 dnf module enable nginx:1.24 -y &>>$logfile
-validate $? "enableing nginx 1.24"
+validate $? "Enabling Nginx:1.24"
 
 dnf install nginx -y &>>$logfile
-validate $? "installing nginx"
+validate $? "Installing Nginx"
+
+systemctl enable nginx  &>>$logfile
+systemctl start nginx 
+validate $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$logfile
-validate $? "removing default"
+validate $? "Removing default content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$logfile
-validate $? "downloading frontend resource"
+validate $? "Downloading frontend"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$logfile
 validate $? "unzipping frontend"
 
+rm -rf /etc/nginx/nginx.conf &>>$logfile
+validate $? "Remove default nginx conf"
+
 cp $scriptdir/nginx.conf /etc/nginx/nginx.conf
-validate $? "copying nginx conf"
+validate $? "Copying nginx.conf"
 
-systemctl restart nginx
-validate $? "restarting nginx"
+systemctl restart nginx 
+validate $? "Restarting nginx"
 
-printtime
-
+print_time
 
 
